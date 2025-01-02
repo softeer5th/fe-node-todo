4e38acf54861b3dcebf67bbd68bc6195aabe7b41
vavoya <vavoya6324@gmail.com>
2025-01-02 18:29:27 +0900

    feat: todo 삭제 interface 구현

--- renders/deleteInterface.js
+++ renders/deleteInterface.js
@@ -1,9 +1,13 @@
 
 
-function deleteTodo() {
+function deleteInterface() {
     const output = [
-        '-----------',
+        '\n\n-----------',
         'todo 삭제',
-        '정말 삭제하시겠습니까? Y/N'
+        '정말 삭제하시겠습니까? Y/N',
+        '-----------',
     ]
+    console.log(output.join('\n'))
 }
+
+module.exports = deleteInterface; // 함수 내보내기
