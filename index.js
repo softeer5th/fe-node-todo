44eca3befc0293518fe9fd91a077c13aa0fc929a
rbgksqkr <rbgks1937@naver.com>
2025-01-02 17:37:26 +0900

    chore: prettier 설정

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const TodoList = require("./TodoList.js");
-const readline = require("readline");
+const TodoList = require('./TodoList.js');
+const readline = require('readline');
 
 let command = null; // 이전에 입력된 명령어(add, print, delete, edit, finish)
 const todoList = new TodoList();
@@ -15,50 +15,50 @@ const COMMAND = {
   delete: 'delete',
   finish: 'finish',
   edit: 'edit',
-}
+};
 
 const INFO_MESSAGE = {
-  start: "[START] 명령어를 입력해주세요: add, print, delete, edit, finish",
-  next: "\n명령어를 입력해주세요: add, print, delete, edit, finish",
+  start: '[START] 명령어를 입력해주세요: add, print, delete, edit, finish',
+  next: '\n명령어를 입력해주세요: add, print, delete, edit, finish',
   add: '추가할 내용을 입력해주세요:',
   delete: '삭제할 todo id를 입력해주세요:',
   finish: '완료한 todo id를 입력해주세요:',
   edit: '수정할 todo id와 수정할 내용을 공백을 두고 입력해주세요:',
-}
+};
 
 const printInfoMessage = (command) => {
-  switch(command) {
+  switch (command) {
     case COMMAND.add:
       console.log(INFO_MESSAGE.add);
       break;
     case COMMAND.delete:
-      console.log(INFO_MESSAGE.delete)
+      console.log(INFO_MESSAGE.delete);
       break;
     case COMMAND.finish:
-      console.log(INFO_MESSAGE.finish)
+      console.log(INFO_MESSAGE.finish);
       break;
     case COMMAND.edit:
-      console.log(INFO_MESSAGE.edit)
+      console.log(INFO_MESSAGE.edit);
       break;
   }
-}
+};
 
 console.log(INFO_MESSAGE.start);
 
-rl.on("line", (line) => {
-  if(command === null && line !== COMMAND.print){
+rl.on('line', (line) => {
+  if (command === null && line !== COMMAND.print) {
     command = line;
     printInfoMessage(command);
     return;
   }
-  if(command === null && line === COMMAND.print){
-      todoList.sort();
-      todoList.print();
-      console.log(INFO_MESSAGE.next);
-      return;
+  if (command === null && line === COMMAND.print) {
+    todoList.sort();
+    todoList.print();
+    console.log(INFO_MESSAGE.next);
+    return;
   }
-  
-  switch(command) {
+
+  switch (command) {
     case COMMAND.add:
       todoList.add(line);
       break;
@@ -77,6 +77,6 @@ rl.on("line", (line) => {
   command = null;
   console.log(INFO_MESSAGE.next);
 });
-rl.on("close", () => {
+rl.on('close', () => {
   process.exit();
 });
