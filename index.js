987ceee67c929e0a7112f769a599f2d83a23effb
rbgksqkr <rbgks1937@naver.com>
2025-01-02 16:00:46 +0900

    refactor: 정렬 및 출력 함수 분리 및 입력 로직 개선

--- index.js
+++ index.js
@@ -13,43 +13,51 @@ let command = null;
 
 const commands = ['add', 'print','delete','edit','finish'];
 
+const sortTodoList = () => {
+  todoList.sort((a, b) => {
+    if(a.isFinished > b.isFinished) return 1;
+    else if(a.isFinished < b.isFinished) return -1;
+    return 0;
+  })
+}
+
+const printTodoList = () => {
+  console.log('id | content')
+  console.log('------------')
+  todoList.forEach(todo => {
+      console.log(` ${todo.id} | ${todo.content} ${todo.isFinished ? '(finished)' : ''}`);
+  });
+}
+
 // 명령어 입력 -> 명령어가 어떤 역할인지 판단 -> 함수 실행
 // add, print, delete, edit, finish
 rl.on("line", (line) => {
-  if(command === null){
-    if(line === 'print'){
-      console.log('id | content')
-      console.log('------------')
-      todoList = todoList.sort((a, b) => {
-        if(a.isFinished > b.isFinished) return 1;
-        else if(a.isFinished < b.isFinished) return -1;
-        return 0;
-      })
+  if(command === null && line !== 'print'){
+    command = line;
+    return;
+  }
+  if(command === null && line === 'print'){
+      sortTodoList();
+      printTodoList();
+      return;
+  }
 
-        todoList.forEach(todo => {
-            console.log(` ${todo.id} | ${todo.content} ${todo.isFinished ? '(finished)' : ''}`);
-        });
-    } else {
-        command = line;
-    }
-  } else if (command === 'add'){
+  if (command === 'add'){
     //add
     todoList.push({id:todoId++,content: line, isFinished: false});
-    command = null;
   } else if (command === 'delete'){
     //delete
     todoList = todoList.filter(todo => todo.id !== Number(line));
-    command = null;
   } else if (command === 'finish'){
     //finish
     todoList = todoList.map(todo => todo.id === Number(line) ? {...todo, isFinished: true} : todo);
-    command = null;
   } else if (command === 'edit'){
     //edit
     const [todoId, editContent] = line.split(' ');
     todoList = todoList.map(todo => todo.id === Number(todoId) ? {...todo, content: editContent} : todo);
-    command = null;
   }
+
+  command = null;
 });
 rl.on("close", () => {
   process.exit();
