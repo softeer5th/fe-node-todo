4f7c91ec7db2d41af20ce4537da8dfe6ec89d424
rbgksqkr <rbgks1937@naver.com>
2025-01-02 17:28:36 +0900

    refactor: 안내 메세지 출력 및 상수화

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const TodoList = require("./TodoList.js");
 const readline = require("readline");
 
+let command = null; // 이전에 입력된 명령어(add, print, delete, edit, finish)
+const todoList = new TodoList();
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -14,18 +17,44 @@ const COMMAND = {
   edit: 'edit',
 }
 
-// 입력된 명령어
-let command = null;
-const todoList = new TodoList();
+const INFO_MESSAGE = {
+  start: "[START] 명령어를 입력해주세요: add, print, delete, edit, finish",
+  next: "\n명령어를 입력해주세요: add, print, delete, edit, finish",
+  add: '추가할 내용을 입력해주세요:',
+  delete: '삭제할 todo id를 입력해주세요:',
+  finish: '완료한 todo id를 입력해주세요:',
+  edit: '수정할 todo id와 수정할 내용을 공백을 두고 입력해주세요:',
+}
+
+const printInfoMessage = (command) => {
+  switch(command) {
+    case COMMAND.add:
+      console.log(INFO_MESSAGE.add);
+      break;
+    case COMMAND.delete:
+      console.log(INFO_MESSAGE.delete)
+      break;
+    case COMMAND.finish:
+      console.log(INFO_MESSAGE.finish)
+      break;
+    case COMMAND.edit:
+      console.log(INFO_MESSAGE.edit)
+      break;
+  }
+}
+
+console.log(INFO_MESSAGE.start);
 
 rl.on("line", (line) => {
   if(command === null && line !== COMMAND.print){
     command = line;
+    printInfoMessage(command);
     return;
   }
   if(command === null && line === COMMAND.print){
       todoList.sort();
       todoList.print();
+      console.log(INFO_MESSAGE.next);
       return;
   }
   
@@ -46,6 +75,7 @@ rl.on("line", (line) => {
   }
 
   command = null;
+  console.log(INFO_MESSAGE.next);
 });
 rl.on("close", () => {
   process.exit();
