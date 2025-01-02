9d832991fc8c2b63bb572d094da81e35a372438d
rbgksqkr <rbgks1937@naver.com>
2025-01-02 16:51:39 +0900

    refactor: 명령어 상수화

--- index.js
+++ index.js
@@ -67,29 +67,36 @@ class TodoList {
   }
 }
 
+const COMMAND = {
+  print: 'print',
+  add: 'add',
+  delete: 'delete',
+  finish: 'finish',
+  edit: 'edit',
+}
 
 // 입력된 명령어
 let command = null;
 const todoList = new TodoList();
 
 rl.on("line", (line) => {
-  if(command === null && line !== 'print'){
+  if(command === null && line !== COMMAND.print){
     command = line;
     return;
   }
-  if(command === null && line === 'print'){
+  if(command === null && line === COMMAND.print){
       todoList.sort();
       todoList.print();
       return;
   }
 
-  if (command === 'add'){
+  if (command === COMMAND.add){
     todoList.add(line);
-  } else if (command === 'delete'){
+  } else if (command === COMMAND.delete){
     todoList.delete(line);
-  } else if (command === 'finish'){
+  } else if (command === COMMAND.finish){
     todoList.finish(line);
-  } else if (command === 'edit'){
+  } else if (command === COMMAND.edit){
     const [todoId, editContent] = line.split(' ');
     todoList.edit(todoId, editContent);
   }
