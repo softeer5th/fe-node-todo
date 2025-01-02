0c6c5f3789b9e1f8b179a3e223d0718d1e005739
pepperdad <becomegoodguy@gmail.com>
2025-01-02 18:03:35 +0900

    feat: 출력 형식 변경 및 투두 로컬 저장 로직 추가
    - todo.json 파일에 투두 저장
    - 나가기 명령어 추가

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 import readline from 'readline';
 import { ToDo } from './todo.js';
+import fs from 'fs';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let toDoList = [];
+let toDoList;
+
+function loadToDoList() {
+    try {
+        const data = fs.readFileSync('todo.json', 'utf8');
+        toDoList = JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading todo.json:', err);
+        toDoList = [];
+    }
+}
 
 async function myInput(text) {
     return new Promise((resolve) => rl.question(text, (answer) => {
-        resolve(answer);
+        resolve(answer.trim());
     }));
 
 }
 
 async function showInformation() {
-    console.log("\n-----TODO LIST에 온 걸 환영해요-----");
-    console.log("1. 투두 보기(명령어: 보기)");
-    console.log("2. 투두 추가(명령어: 추가)");
-    console.log("3. 투두 삭제(명령어: 삭제)");
-    console.log("4. 투두 상태 바꾸기(명령어: 바꾸기)");
+    console.log("\n-----TODO LIST에 온 걸 환영해요-----\n");
+    showToDo();
+    console.log("------------------------------------");
+    console.log("1. 투두 추가(명령어: 추가)");
+    console.log("2. 투두 삭제(명령어: 삭제)");
+    console.log("3. 투두 상태 바꾸기(명령어: 바꾸기)");
+    console.log("4. 나가기(명령어: 나가기)");
     console.log("------------------------------------");
 
     return await myInput("원하는 명령어를 입력해주세요: ");
@@ -38,10 +51,10 @@ function showToDo() {
     }
     else {
         toDoList.map((toDo) => {
-            console.log(`\n${toDo.id} ${toDo.checked ? '✅' : '❌'}`);
+            console.log(`${toDo.id} ${toDo.checked ? '✅' : '❌'}`);
             console.log('제목:', toDo.title);
             console.log('내용:', toDo.content);
-            console.log('생성 일자:', toDo.createdAt);
+            console.log('생성 일자:', toDo.createdAt, "\n");
         })
     }
 
@@ -79,18 +92,23 @@ async function convertToDo() {
     });
 };
 
+function saveToDo() {
+    const data = JSON.stringify(toDoList);
+
+    fs.writeFileSync('todo.json', data, 'utf8');
+}
+
 async function main() {
+    loadToDoList();
+
     while (true) {
         const value = await showInformation();
 
         switch (value) {
-            case 'q':
+            case '나가기':
                 process.exit();
-            case '보기':
-                showToDo();
-                break;
             case '추가':
-                await addTodo();
+                await addToDo();
                 break;
             case '삭제':
                 await deleteToDo();
@@ -99,8 +117,10 @@ async function main() {
                 await convertToDo();
                 break;
             default:
-            // console.log(value);
+               console.log('유효한 명령어를 입력해주세요.');
         }
+
+        saveToDo();
     }
     rl.close();
 }
